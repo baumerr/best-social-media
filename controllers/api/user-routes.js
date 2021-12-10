8c3a6d3669f39cf53accf7876143cfb08925e902
randy <baumer.randy@gmail.com>
2021-12-10 13:11:41 -0500

    added user routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req, res) => {
     User.findAll({
@@ -101,3 +102,54 @@ router.post('/login', (req, res) => {
           });
       })
 })
+
+router.post('/logout', (req, res) => {
+    if(req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+      .then(dbUserData => {
+          if(!dbUserData) {
+              res.status(404).json({ message: 'No user found with this ID' });
+              return;
+          }
+          res.json(dbUserData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+      .then(dbUserData => {
+          if(!dbUserData) {
+              res.status(404).json({ message: 'No user found with this ID' });
+              return;
+          }
+          res.json(dbUserData);
+      })
+      .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+      });
+});
+
+module.exports = router;
